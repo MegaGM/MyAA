5b52adadd05e9f78afaf94030483364dee6a502a
Mega <Aza.Noriega@gmail.com>
2019-01-27 05:39:32 +0500

    app.requestSingleInstanceLock()

--- src/main/index.js
+++ src/main/index.js
@@ -6,25 +6,30 @@ const
   { exec } = require('child_process'),
   { app, BrowserWindow, ipcMain, Tray, Menu, globalShortcut } = require('electron')
 let
-  w, // BrowserWindow
+  w, // w: BrowserWindow
   tray
 // API = require('../renderer/api')
-
-function showHideWindow() {
-  if (w.isVisible())
-    w.hide()
-  else {
-    w.show()
-    w.focus()
-  }
-}
-
-
-app.commandLine.appendSwitch('ignore-gpu-blacklist')
 
 
 // nani sore? 
 // app.dock.hide()
+app.commandLine.appendSwitch('ignore-gpu-blacklist')
+
+{
+  /**
+   * Focus the window instead of launching second app
+   */
+  const firstInstance = app.requestSingleInstanceLock()
+  if (!firstInstance)
+    app.quit()
+  else
+    app.on('second-instance', (event, commandLine, workingDirectory) => {
+      if (w.isMinimized())
+        w.restore()
+      w.show()
+      w.focus()
+    })
+}
 
 app.on('activate', () => w === null && createWindow())
 app.on('window-all-closed', () => process.platform !== 'darwin' && app.quit())
@@ -43,8 +48,21 @@ app.once('ready', () => {
 
   setupTray()
   createWindow()
+  devSetup()
 })
 
+
+/**
+ * Flow and Utils
+ */
+function showHideWindow() {
+  if (w.isVisible())
+    w.hide()
+  else {
+    w.show()
+    w.focus()
+  }
+}
 
 function createWindow() {
   const wOptions = {
@@ -71,7 +89,7 @@ function createWindow() {
 
   w.on('move', saveWindowPosition)
   w.on('resize', saveWindowPosition)
-  w.on('blur', () => w.hide())
+  // w.on('blur', () => w.hide())
   w.on('closed', () => w = null)
   w.once('ready-to-show', () => {
     w.show()
@@ -81,9 +99,6 @@ function createWindow() {
   function saveWindowPosition(e) {
     // console.info('saveWindowPosition STUB, w.getBounds(): ', w.getBounds())
   }
-
-  if (process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase().startsWith('dev'))
-    w.webContents.openDevTools()
 }
 
 function setupTray() {
@@ -111,10 +126,31 @@ function setupTray() {
   ]))
 }
 
+function devSetup() {
+  const env = process.env.NODE_ENV
+  if (!env || !env.toLowerCase().startsWith('dev'))
+    return
+
+  {
+    /**
+     * Extensions
+     */
+    const { default: installExtension, VUEJS_DEVTOOLS } = require('electron-devtools-installer')
+    installExtension(VUEJS_DEVTOOLS)
+    BrowserWindow.removeDevToolsExtension('Lubuntu Scrollbars')
+    const installedExtensions = BrowserWindow.getDevToolsExtensions()
+    console.info('installedExtensions: ', installedExtensions)
+  }
+
+  w.webContents.openDevTools()
+}
+
+
 async function execAsync(command) {
-  let child = exec(command)
+  const child = exec(command)
+
   return new Promise((resolve, reject) => {
-    let { stdout, stderr } = child
+    const { stdout, stderr } = child
     stdout.on('data', console.info)
     stderr.on('data', console.error)
     // child.on('close', code => console.info('closing code: ' + code))
@@ -122,4 +158,3 @@ async function execAsync(command) {
     child.addListener('exit', resolve)
   })
 }
-

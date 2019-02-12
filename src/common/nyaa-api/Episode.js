7f0f73bcea6be007ef5dff87e5934827326953ed
Mega <Aza.Noriega@gmail.com>
2019-02-13 00:33:16 +0500

    better regexp to parse NyaaEpisode title

--- src/common/nyaa-api/Episode.js
+++ src/common/nyaa-api/Episode.js
@@ -27,22 +27,30 @@ class Episode {
    * [HorribleSubs] Tensei Shitara Slime Datta Ken - 17 [720p].mkv
    * [project-gxs] Uchuu Senkan Yamato 2202 - 22 [10bit 1080p] [6718054B].mkv
    * [Erai-raws] Kaguya-sama wa Kokurasetai - Tensai-tachi no Renai Zunousen - 05 [1080p][Multiple Subtitle].mkv
+   * [Nauti] Kakegurui XX - 01 [5BD79EEA].mkv [720p]
    */
   static parseTitle(rawTitle) {
     const
       subTeams = diffMap.map(d => d.subTeam).filter(Boolean),
       uniqueSubTeams = [...new Set(subTeams)],
-      regex = new RegExp(`\\[(${uniqueSubTeams.join('|')})] (.+) - (\\d+) \\[(?:[^\\]]+)?(1080p|720p|480p)]`, 'i')
+      mainRegexp = new RegExp(`\\[(${uniqueSubTeams.join('|')})] (.+) - (\\d+)`, 'i'),
+      qualityRegexp = /\[(?:[^\]]+)?(1080p|720p|480p)(?:[^\]]+)?]/i
 
-    let parsedTitle = null
-    rawTitle.replace(regex, (match, $1, $2, $3, $4) => {
-      parsedTitle = {
-        subTeam: $1,
-        title: $2,
-        episodeNumber: +$3,
-        quality: $4,
-      }
-    })
+    if (!mainRegexp.test(rawTitle))
+      return null // indicate failure
+
+    const [match, subTeam, title, episodeNumber] = rawTitle.match(mainRegexp)
+
+    const parsedTitle = {
+      subTeam,
+      title,
+      episodeNumber: +episodeNumber,
+    }
+
+    if (qualityRegexp.test(rawTitle)) {
+      const [match, quality] = rawTitle.match(qualityRegexp)
+      parsedTitle.quality = quality
+    }
 
     return parsedTitle
   }
